760cae9f91b7004f51fb8a26354174d91833055f
Nicholas Soares <nsoares22@lasalle-academy.org>
2024-03-12 16:07:35 -0400

    Completed Quizzes

--- quiz/readUsers.js
+++ quiz/readUsers.js
@@ -1,2 +1,18 @@
 const express = require('express')
 const router = express.Router();
+
+router.get('/usernames', (req, res) => {
+  let usernames = req.users.map(function(user) {
+    return {id: user.id, username: user.username};
+  });
+  res.send(usernames);
+});
+
+app.get('/username/:name', (req, res) => {
+  let name = req.params.name
+  let foundUser = req.users.find((user) => user.username == name)
+  if (foundUser)
+    res.send(foundUser)
+  else 
+  res.send({error: "User not found"})
+})
